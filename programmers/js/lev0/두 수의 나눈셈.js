72ebd78c5cf476eed3231e5225f47956a0b02576
jonejtwojthree <whd4767@naver.com>
2023-09-11 02:22:33 +0900

    두  수의 나눗셈

--- programmers/js/lev0/두 수의 나눈셈.js
+++
@@ -1,9 +0,0 @@
-function solution(num1, num2) {
-  // floor 내림 , trunc 버림
-  var answer = Math.floor((num1 / num2) * 1000);
-  // var answer = Math.trunc((num1 / num2) * 1000);
-
-  return answer;
-}
-
-console.log(solution(1, 16));
