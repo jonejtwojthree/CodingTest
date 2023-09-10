c0ed1b59575bb24939a00c0c82198bec0381d59c
jongwonseo <whd7327@naver.com>
2023-09-11 02:24:35 +0900

    머쓱이보다 키 큰 사람

--- programmers/java/lev0/머쓱이보다 키 큰 사람.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-class Solution {
-    public int solution(int[] array, int height) {
-        return Arrays.stream(array).filter(el->el>height).toArray().length;
-
-    }
-}
