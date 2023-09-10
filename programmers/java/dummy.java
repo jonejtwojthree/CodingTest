b026f71af9d078bfd02276ddf21f0c8bbf248e53
jongwonseo <whd7327@naver.com>
2023-09-10 23:50:49 +0900

    숫자 비교하기

---
+++ programmers/java/dummy.java
@@ -0,0 +1,7 @@
+package programmers.java;
+
+public class dummy {
+    public static void main(String[] args){
+        System.out.println("qwer");
+    }
+}
---
+++ programmers/js/숫자 비교하기.js
@@ -0,0 +1,6 @@
+function solution(num1, num2) {
+  var answer = 0;
+  return num1 == num2 ? 1 : -1;
+}
+
+console.log(solution(1, 1));
