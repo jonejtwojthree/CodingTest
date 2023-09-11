4b67b16289a3c9b3bc843943f6ae2a2a0391d874
jonejtwojthree <whd4767@naver.com>
2023-09-11 18:11:17 +0900

    all

--- programmers/js/lev0/아이스 아메리카노.js
+++
@@ -1,5 +0,0 @@
-function solution(money) {
-  //   var cup = ~~(money / 5500);
-  //   return [cup, money - cup * 5500];
-  return [Math.floor(money / 5500), money % 5500];
-}
--- programmers/js/lev0/양꼬치.js
+++
@@ -1,4 +0,0 @@
-function solution(n, k) {
-  return n * 12000 + (k - Math.floor(n / 10)) * 2000;
-  //   return n * 12000 + (k - ~~(n / 10)) * 2000;
-}
