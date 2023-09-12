6fcda8a30edaf90f225d0aac06b298a8362ddf0c
jonejtwojthree <whd4767@naver.com>
2023-09-12 15:32:33 +0900

    문자 반복 출력하기 (구조 분해 할당!!!!!)

--- programmers/js/lev0/문자 반복 출력하기.js
+++ programmers/js/lev0/문자 반복 출력하기.js
@@ -4,3 +4,8 @@ function solution(my_string, n) {
     .map((el) => el.repeat(n))
     .join("");
 }
+
+// 구조분해할당!!!!!!!!!!!!!!!!
+function solution(my_string, n) {
+  return [...my_string].map((el) => el.repeat(n)).join("");
+}
