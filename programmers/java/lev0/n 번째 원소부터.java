9753106dfb86c32d4430e2866a1801b2ef4cb906
jonejtwojthree <whd4767@naver.com>
2023-09-13 23:14:35 +0900

    n 번째 원소부터

---
+++ programmers/java/lev0/n 번째 원소부터.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        return Arrays.copyOfRange(num_list, n-1, num_list.length);
+    }
+}
